5c91afd52b989cbcb00962d49d8855c8a7b047dc
techatlasdev <srgoqui@gmail.com>
2024-09-30 18:40:19 -0500

    Solucionando error de dependencias

--- backend/reflecta/api/apps.py
+++ backend/reflecta/api/apps.py
@@ -1,7 +1,6 @@
 import os
 import ee
 import json
-import base64
 from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
@@ -11,19 +10,32 @@ class ApiConfig(AppConfig):
     def ready(self):
         # Inicializar Google Earth Engine (EE)
         try:
-            # Cargar las variables de entorno
-            service_account = os.environ.get('SERVICE_ACCOUNT')
-            credentials_base64 = os.environ.get('GOOGLE_CREDENTIALS_JSON')
+            # Cargar las variables de entorno con valores por defecto
+            service_account = os.environ.get('SERVICE_ACCOUNT', 'default_service_account')
+            project_id = os.environ.get('PROJECT_ID', 'reflecta-techatlasdev')
+            private_key_id = os.environ.get('PRIVATE_KEY_ID', 'default_private_key_id')
+            private_key = os.environ.get('PRIVATE_KEY', '-----BEGIN PRIVATE KEY-----\nYOUR_PRIVATE_KEY_HERE\n-----END PRIVATE KEY-----\n')
+            client_email = os.environ.get('CLIENT_EMAIL', 'default_client_email')
+            client_id = os.environ.get('CLIENT_ID', 'default_client_id')
+            auth_uri = os.environ.get('AUTH_URI', 'https://accounts.google.com/o/oauth2/auth')
+            token_uri = os.environ.get('TOKEN_URI', 'https://oauth2.googleapis.com/token')
+            auth_provider_cert_url = os.environ.get('AUTH_PROVIDER_CERT_URL', 'https://www.googleapis.com/oauth2/v1/certs')
+            client_cert_url = os.environ.get('CLIENT_CERT_URL', 'https://www.googleapis.com/robot/v1/metadata/x509/default_client_email')
 
-            if not service_account or not credentials_base64:
-                raise ValueError("Se requiere la variable de entorno 'SERVICE_ACCOUNT' y 'GOOGLE_CREDENTIALS_JSON'")
-
-            # Decodificar el JSON de las credenciales desde Base64
-            credentials_json = base64.b64decode(credentials_base64.encode()).decode('utf-8')
-            credentials_dict = json.loads(credentials_json)
-            
             # Crear las credenciales de servicio
-            credentials = ee.ServiceAccountCredentials(service_account, key_data=json.dumps(credentials_dict))
+            credentials = ee.ServiceAccountCredentials(service_account, key_data=json.dumps({
+                "type": "service_account",
+                "project_id": project_id,
+                "private_key_id": private_key_id,
+                "private_key": private_key,
+                "client_email": client_email,
+                "client_id": client_id,
+                "auth_uri": auth_uri,
+                "token_uri": token_uri,
+                "auth_provider_x509_cert_url": auth_provider_cert_url,
+                "client_x509_cert_url": client_cert_url,
+                "universe_domain": "googleapis.com"
+            }))
 
             # Inicializar Earth Engine
             ee.Initialize(credentials)
