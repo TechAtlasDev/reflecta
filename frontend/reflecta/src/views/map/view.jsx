04ddc6f1a92cab4bac4bdf626e129666087dcca8
techatlasdev <srgoqui@gmail.com>
2024-10-01 16:51:24 -0500

    Mejorando interfaz

--- frontend/reflecta/src/views/map/view.jsx
+++ frontend/reflecta/src/views/map/view.jsx
@@ -6,46 +6,66 @@ import {
   Popup,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useState, useEffect } from "react";
-import L, { marker } from "leaflet"; // Importa Leaflet para crear el icono personalizado
+import { useState, useEffect, useRef } from "react";
+import L from "leaflet"; // Importa Leaflet para crear el icono personalizado
 import Interfaz from "./interfaz";
 import GetCordinates from "./functions/locationLandsat";
 
 // Define el icono personalizado
 const landsatIcon = L.icon({
-  iconUrl: "/icons/landsat.png", // Cambia esta ruta a la imagen que desees
-  iconSize: [32, 32], // Tamaño del icono en píxeles
-  iconAnchor: [16, 32], // El punto de anclaje del icono (base del icono)
-  popupAnchor: [0, -32], // El punto de anclaje para los popups
+  iconUrl: "/icons/landsat.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
 });
 
 const markerIcon = L.icon({
-  iconUrl: "/icons/marker.png", // Cambia esta ruta a la imagen que desees
-  iconSize: [32, 32], // Tamaño del icono en píxeles
-  iconAnchor: [16, 32], // El punto de anclaje del icono (base del icono)
-  popupAnchor: [0, -32], // El punto de anclaje para los popups
+  iconUrl: "/icons/marker.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+const pinIcon = L.icon({
+  iconUrl: "/icons/pin.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
 });
 
 function HandlerMap() {
   const [clickedPosition, setClickedPosition] = useState(null);
+  const markerRef = useRef(null); // Referencia para el marcador
 
   // Registrar el evento de clic en el mapa
   useMapEvents({
     click: (event) => {
       setClickedPosition(event.latlng); // Guarda la latitud y longitud del clic
+
+      // Si el marcador ya está creado, abrir automáticamente el popup
+      if (markerRef.current) {
+        markerRef.current.openPopup();
+      }
     },
   });
 
   return clickedPosition === null ? null : (
-    // Si hay una posición de clic, muestra un marcador
-    <Marker position={clickedPosition}>
-      <Popup>Aquí estás</Popup>
+    // Si hay una posición de clic, muestra un marcador con el popup abierto automáticamente
+    <Marker position={clickedPosition} ref={markerRef} icon={pinIcon}>
+      <Popup autoPan={false}>
+        <button
+          className='btn btn-xs btn-outline btn-success'
+          onClick={() => alert("Botón presionado")}
+        >
+          Notificar cuando Landsat pase aquí
+        </button>
+      </Popup>
     </Marker>
   );
 }
 
 function Map() {
-  const [landsatCoordinates, setLandsatCoordinates] = useState(null); // Inicialmente null
+  const [landsatCoordinates, setLandsatCoordinates] = useState(null);
   const [userCoordinates, setUserCoordinates] = useState(null);
 
   // Función para obtener las coordenadas del usuario
@@ -76,7 +96,6 @@ function Map() {
 
   useEffect(() => {
     DrawLandsat();
-
     getUserLocation(); // Obtener la ubicación del usuario
   }, []); // Ejecutar solo una vez al montar el componente
 
@@ -87,7 +106,7 @@ function Map() {
         center={[10, 10]} // Coordenadas iniciales
         zoom={2} // Nivel de zoom inicial
         className='w-screen h-screen absolute z-0'
-        zoomControl={false} // Deshabilita los controles de zoom predeterminados
+        zoomControl={false}
       >
         <TileLayer
           url='https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGVjaGF0bGFzZGV2IiwiYSI6ImNtMWw2anpnZTAyZG8ybm9rYjdsdmI0a2IifQ.e3n0BC8mLsl5rYgx9mBQKQ'
@@ -106,7 +125,7 @@ function Map() {
           >
             <Popup>
               <b>¡El Landsat 9 se encuentra aquí!</b>
-              <br></br> Coordenadas: {landsatCoordinates.latitude},{" "}
+              <br /> Coordenadas: {landsatCoordinates.latitude},{" "}
               {landsatCoordinates.longitude}
             </Popup>
           </Marker>
@@ -115,11 +134,17 @@ function Map() {
         {userCoordinates && (
           <Marker
             position={[userCoordinates.latitude, userCoordinates.longitude]}
-            icon={markerIcon} // Usa el icono personalizado para la ubicación del usuario
+            icon={markerIcon}
           >
             <Popup>
               Tu ubicación: {userCoordinates.latitude},{" "}
               {userCoordinates.longitude}
+              <button
+                className='btn btn-xs btn-outline btn-success'
+                onClick={() => alert("Botón presionado")}
+              >
+                Notificar cuando Landsat pase aquí
+              </button>
             </Popup>
           </Marker>
         )}
