1f46192a2ba4ccb2ecc70da96c0ed7e397bdb5ca
techatlasdev <srgoqui@gmail.com>
2024-09-30 19:10:18 -0500

    Añadiendo integración con API

--- frontend/reflecta/src/views/map/functions/locationLandsat.jsx
+++ frontend/reflecta/src/views/map/functions/locationLandsat.jsx
@@ -2,7 +2,7 @@
 
 export default async function GetCordinates() {
   const response = await fetch(
-    "http://localhost:8000/api/landsat/landsat-coordinates"
+    "https://reflecta-wimi.vercel.app/api/landsat/landsat-coordinates"
   );
   const data = await response.json();
   return data;
