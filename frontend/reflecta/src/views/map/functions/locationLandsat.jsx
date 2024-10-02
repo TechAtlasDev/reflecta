790a41bdfcb2e5f6e62bc638baa709f397b08390
techatlasdev <srgoqui@gmail.com>
2024-10-01 22:06:30 -0500

    Solucionando conflictos entre backend y frontend

--- frontend/reflecta/src/views/map/functions/locationLandsat.jsx
+++ frontend/reflecta/src/views/map/functions/locationLandsat.jsx
@@ -1,5 +1,5 @@
 // Obteniendo las coordenadas del Landsat9
-import { urlAPI } from "@/lib/variables";
+import { urlAPI } from "@/components/variables";
 
 export default async function GetCordinates() {
   const response = await fetch(`${urlAPI}/landsat9/coordinates/`);
