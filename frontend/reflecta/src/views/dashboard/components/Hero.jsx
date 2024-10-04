096efb4c4fe7e48e73ebc85e6984c32f0a8fd403
techatlasdev <srgoqui@gmail.com>
2024-10-04 12:32:22 -0500

    Añadiendo sparkles -> Especto visual

--- frontend/reflecta/src/views/dashboard/components/Hero.jsx
+++ frontend/reflecta/src/views/dashboard/components/Hero.jsx
@@ -2,10 +2,37 @@ import GridIntegrations from "./GridIntegrations";
 import ItemIntegrations from "./ItemIntegrations";
 import Title from "@/components/Title";
 
+import { SparklesCore } from "@/components/Sparkletitle";
+
 export default function Hero() {
   return (
-    <section className='flex flex-col items-center mt-10 gap-10 my-20'>
-      <Title text='Landsat 9' />
+    <section className='flex flex-col items-center mt-10 gap-10 w-full'>
+      <div className='w-full my-10 flex flex-col items-center justify-center overflow-hidden rounded-md'>
+        <h1 className='md:text-7xl text-7xl lg:text-9xl font-bold text-center text-white relative z-20'>
+          Landsat 9
+        </h1>
+        <div className='w-[40rem] h-40 relative'>
+          {/* Gradients */}
+          <div className='absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm' />
+          <div className='absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4' />
+          <div className='absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm' />
+          <div className='absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4' />
+
+          {/* Core component */}
+          <SparklesCore
+            background='transparent'
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className='w-full h-full'
+            particleColor='#fff'
+          />
+
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className='absolute inset-0 w-full h-full bg-slate-950 [mask-image:radial-gradient(350px_200px_at_top,transparent_10%,white)]'></div>
+        </div>
+      </div>
+
       <GridIntegrations>
         <ItemIntegrations
           to='/dashboard/usos'
--- frontend/reflecta/src/views/dashboard/view.jsx
+++ frontend/reflecta/src/views/dashboard/view.jsx
@@ -3,9 +3,7 @@ import Hero from "./components/Hero";
 
 export default function DashboardPage() {
   return (
-    <main class='relative min-h-screen max-w-screen overflow-x-hidden bg-slate-950'>
-      <div class='absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]'></div>
-      <div class='absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]'></div>
+    <main class='min-h-screen max-w-screen overflow-x-hidden bg-slate-950'>
       <Navbar />
       <Hero />
     </main>
