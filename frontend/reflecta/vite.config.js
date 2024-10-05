a6f5765387ad3e713db73d6651f204711eeab908
Meristyn <mcallepinto@gmail.com>
2024-10-05 13:22:25 -0500

    Update vite.config.js

--- frontend/reflecta/vite.config.js
+++ frontend/reflecta/vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mi PWA',
+        short_name: 'PWA',
+        description: 'Esta es una Progressive Web App.',
+        theme_color: '#56368D',
+        background_color: '#56368D',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/reflecta/public/logos/teamColor.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/reflecta/public/logos/teamColor.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
